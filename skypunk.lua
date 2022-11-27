0c8f60ca0e6b914a305e4fdbf8b30db6224ac8f4
Matthew Riche <matthew.riche@gmail.com>
2022-11-26 22:59:00 -0500

    Made sprite size variable, made multiple anim sequences possible

--- skypunk.lua
+++ skypunk.lua
@@ -17,17 +17,20 @@ end
 
 -- Entity master table and class
 ents = {}
-function ents:new(type, mind, newsprite, newx, newy)
+function ents:new(newsprite, newsize, newmind, newx, newy)
 
 	-- Fresh ent table; basic stats
 	ent = {
 		sprite = newsprite, -- Type ID (inexorable from type)
-		mind = 0, -- control ID, zero is "playable"
-		drawframe = 0, -- Which frame of animation should draw now.
-		playframes = {0, 1, 2}, -- Which frames to offset from the sprite.
+		size = newsize,
+		mind = newmind, -- control ID, zero is "playable"
+		drawframe = newsprite, -- Which frame of animation should draw now.
+		animations ={ {0, 1, 0, 2} }, -- Multiple "reels" of animation.
+		anim_reel = 1, -- Which "reel" from animations to play.
+		--playframes = {0, 1, 0, 2, 0}, -- Which frames to offset from the sprite.
 		animframe = 0, -- Which in the play frames sequence are we drawing.
 		tick = 0, -- Tick timer every in-game frame to set anim speed.
-		animspeed = 5, -- Frames in engine until animation next frame
+		animspeed = 20, -- Frames in engine until animation next frame
 		x = newx, -- X screen location
 		y = newy, -- Y screen location
 		x_vel = 0.0, -- horizontal velocity
@@ -43,23 +46,18 @@ function ents:new(type, mind, newsprite, newx, newy)
 			self.tick = 0
 			-- the playframes table has a list of offsets, found with animframe.
 			-- First make sure we haven't counted past the last frame:
-			if(self.animframe > len(self.playframes)) then
+			if(self.animframe > len(self.animations[self.anim_reel])) then
 				self.animframe = 1
 			end
-			-- If the animframe is witin bounds, set which frame to draw
-			self.drawframe = self.sprite + self.playframes[self.animframe]
-			-- If we pull 'nil' from the playframe list, then it's ran out.
-			if(self.drawframe == nil) then
-				-- If the play frame list has ran out, we move ahead.
-				self.animframe = 1
-				self.drawframe = self.sprite + self.playframes[self.animframe]
-			end
+	
+			-- Drawing the frame offset from the playframes list, add it to the spr index
+			self.drawframe = self.sprite + (self.animations[self.anim_reel][self.animframe] * self.size)
 		end
 	end
 	function ent:draw()
 		-- Render the sprite with the Consoles draw command.
 		-- Tic80 syntax
-		spr(self.drawframe, self.x, self.y, 2, 2)
+		spr(self.drawframe, self.x, self.y, nil, 1, nil, nil, self.size, self.size)
 	end
 	table.insert(self, ent) -- Put self new ent in the ents list.
 end
@@ -67,13 +65,18 @@ end
 
 function init()
 	-- make a player in the ent table:
-	ents:new(0, 0, 0, 20, 20)
+	ents:new(256, 2, 0, 0, 20, 20)
 end
+
 
 print("Initializing!", 10, 10, 15)
 init()
 
+
 function TIC()
+
+	-- clear the screen
+	cls(10)
 
 	for i, e in ipairs(ents) do
 		ents[i]:animate()
@@ -93,18 +96,18 @@ end
 -- </TILES>
 
 -- <SPRITES>
--- 001:0000000000000002000000cc000000cc000000dc000000dd099000dd9999000d
--- 002:2ccd00002cccd0002cccc000cc330000c2112000d26f20002222100c3223000c
--- 003:000000000000000000000002000000cc000000cc000000dc000000dd099000dd
--- 004:000000002ccd00002cccd0002cccc000cc330000c2112000d26f200c2222100c
--- 005:000000000000000000000002000000cc000000cc000000dc000000dd09900008
--- 006:000000002ccd00002cccd0002cccc000cc330000c2112000888f200c8888000c
--- 017:99990999aaa99aaa099999990008898800000888000000000000000000000000
--- 018:9999900caaaaa9dd9999990c8888980c8888800cfccf0000fccf0000ffff0000
--- 019:9999099d99999999aaaa98aa0aa889aa0000088a000000000000000000000000
--- 020:3223900c999999ddaaaa890caaaa980caaaa800caaaf0000ffff000000000000
--- 021:9889098888889888099999990008898800000888000000000000000000000000
--- 022:8888900c8888a9dd9999990cfeef980cfccf800cfccf0000ffff000000000000
+-- 000:0000000000000002000000cc000000cc000000dc000000dd099000dd9999000d
+-- 001:2ccd00002cccd0002cccc000cc330000c2112000d26f20002222100c3223000c
+-- 002:000000000000000000000002000000cc000000cc000000dc000000dd099000dd
+-- 003:000000002ccd00002cccd0002cccc000cc330000c2112000d26f200c2222100c
+-- 004:000000000000000000000002000000cc000000cc000000dc000000dd09900008
+-- 005:000000002ccd00002cccd0002cccc000cc330000c2112000888f200c8888000c
+-- 016:99990999aaa99aaa099999990008898800000888000000000000000000000000
+-- 017:9999900caaaaa9dd9999990c8888980c8888800cfccf0000fccf0000ffff0000
+-- 018:9999099d99999999aaaa98aa0aa889aa0000088a000000000000000000000000
+-- 019:3223900c999999ddaaaa890caaaa980caaaa800caaaf0000ffff000000000000
+-- 020:9889098888889888099999990008898800000888000000000000000000000000
+-- 021:8888900c8888a9dd9999990cfeef980cfccf800cfccf0000ffff000000000000
 -- </SPRITES>
 
 -- <WAVES>
