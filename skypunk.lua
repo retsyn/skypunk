a96e8b10496406ee56cbe30237e4afc067e3df86
Matthew Riche <matthew.riche@gmail.com>
2022-11-27 21:48:30 -0500

    Animations triggered and alterable.  Control impulses working

--- skypunk.lua
+++ skypunk.lua
@@ -6,36 +6,88 @@
 -- version: 0.1
 -- script:  lua
 
+-- enumerated constants:
+entity_ids = {
+	skypunk = 256,
+}
+
+minds = {
+	player = 0,
+}
+
 -- lua table-size util
-function len(table)
-	size = 0
+function Len(table)
+	local size = 0
 	for _ in pairs(table) do
 		size = size + 1
 	end
 	return size
 end
 
+-- Animation data for each object:
+Animation_data = {
+	skypunk = {{0}, {1}, {2}}, -- straight, up, down
+}
+
+
+function Diminish(float, factor)
+	-- function to move towards zero by a given factor, but not past it.  Good for friction.
+	if(float > 0) then
+		if(math.abs(float) < factor) then 
+			float = 0
+		else
+			float = float - factor
+		end
+	elseif(float < 0) then
+		if(math.abs(float) < factor) then
+			float = 0
+		else
+			float = float + factor
+		end
+	end
+	return float
+end
+
+
 -- Entity master table and class
 ents = {}
-function ents:new(newsprite, newsize, newmind, newx, newy)
-
+function ents:new(newsprite, newsize, newmind, newx, newy, new_anims)
+	-- Process optional args
+	if(new_anims == nil) then
+		new_anims = {{0, 1, 0, 2}}
+	end
 	-- Fresh ent table; basic stats
 	ent = {
 		sprite = newsprite, -- Type ID (inexorable from type)
 		size = newsize,
 		mind = newmind, -- control ID, zero is "playable"
 		drawframe = newsprite, -- Which frame of animation should draw now.
-		animations ={ {0, 1, 0, 2} }, -- Multiple "reels" of animation.
+		animations = new_anims, -- Multiple "reels" of animation.
 		anim_reel = 1, -- Which "reel" from animations to play.
-		--playframes = {0, 1, 0, 2, 0}, -- Which frames to offset from the sprite.
 		animframe = 0, -- Which in the play frames sequence are we drawing.
 		tick = 0, -- Tick timer every in-game frame to set anim speed.
 		animspeed = 20, -- Frames in engine until animation next frame
 		x = newx, -- X screen location
 		y = newy, -- Y screen location
+
+		-- Physics stuff
 		x_vel = 0.0, -- horizontal velocity
 		y_vel = 0.0, -- vertical velocity
+		airspeed = 0.5,
+		airfriction = 0.3,
+		maxairspeed = 0.8,
+		impulses = {
+			up=false,
+			down=false,
+			left=false,
+			right=false,
+			attack=false,
+			dodge=false,
+			special=false,
+		} -- impulses are "intentions" of an entity, whether from ai 
+		-- or player's controller input.
 	}
+
 	-- Functions that work on all ents:
 	function ent:animate()
 		-- Iterate the ticker to see if it's time to advance a frame.
@@ -46,18 +98,91 @@ function ents:new(newsprite, newsize, newmind, newx, newy)
 			self.tick = 0
 			-- the playframes table has a list of offsets, found with animframe.
 			-- First make sure we haven't counted past the last frame:
-			if(self.animframe > len(self.animations[self.anim_reel])) then
+			if(self.animframe > Len(self.animations[self.anim_reel])) then
 				self.animframe = 1
 			end
-	
 			-- Drawing the frame offset from the playframes list, add it to the spr index
 			self.drawframe = self.sprite + (self.animations[self.anim_reel][self.animframe] * self.size)
 		end
+
+		-- how we animate the player.
+		if(self.sprite == entity_ids.skypunk) then
+			if(self.impulses.up == true and self.impulses.down == false) then
+				self.anim_reel = 3
+				self.tick = self.animspeed
+			elseif(self.impulses.down == true and self.impulses.up == false) then
+				self.anim_reel = 2
+				self.tick = self.animspeed
+			else
+				self.anim_reel = 1
+				self.tick = self.animspeed
+			end
+		end
+
 	end
+
+
+	function ent:think()
+		-- Make it think!  Also, for think=0, get player input.
+
+		-- clean out all impulses:
+		for i,e in ipairs(self.impulses) do
+			self.impulses[i] = false
+		end
+
+		-- PLAYER!
+		if(self.mind == minds.player) then
+			self.impulses.up = btn(0)
+			self.impulses.down = btn(1)
+			self.impulses.left = btn(2)
+			self.impulses.right = btn(3)
+			self.impulses.attack = btnp(4)
+			self.impulses.dodge = btnp(5)
+			self.impulses.special = btnp(6)
+		end
+	end
+
+
+	function ent:act()
+		-- Make it act on the impulses, from AI or controller alike.
+		if(self.impulses.up) then self.y_vel = self.y_vel - self.airspeed end
+		if(self.impulses.down) then self.y_vel = self.y_vel + self.airspeed end 
+		if(self.impulses.left) then self.x_vel = self.x_vel - self.airspeed end
+		if(self.impulses.right) then self.x_vel = self.x_vel + self.airspeed end
+	end
+
+
+	function ent:phys()
+		-- Apply velocities, friction, and collision for an ent.
+
+		-- Cap velocity at top speed:
+		if(self.x_vel > self.maxairspeed) then 
+			self.x_vel = self.maxairspeed 
+		elseif(self.x_vel < -self.maxairspeed) then
+			self.x_vel = -self.maxairspeed
+		end
+		if(self.y_vel > self.maxairspeed) then 
+			self.y_vel = self.maxairspeed 
+		elseif(self.y_vel < -self.maxairspeed) then
+			self.y_vel = -self.maxairspeed
+		end
+		
+		-- Stack the velocity on:
+		self.x = self.x + self.x_vel
+		self.y = self.y + self.y_vel
+
+		-- Apply friction.
+		self.x_vel = Diminish(self.x_vel, self.airfriction)
+		self.y_vel = Diminish(self.y_vel, self.airfriction)
+
+	end
+
+
 	function ent:draw()
 		-- Render the sprite with the Consoles draw command.
 		-- Tic80 syntax
 		spr(self.drawframe, self.x, self.y, nil, 1, nil, nil, self.size, self.size)
+
 	end
 	table.insert(self, ent) -- Put self new ent in the ents list.
 end
@@ -65,21 +190,21 @@ end
 
 function init()
 	-- make a player in the ent table:
-	ents:new(256, 2, 0, 0, 20, 20)
+	ents:new(entity_ids.skypunk, 2, minds.player, 20, 30, Animation_data.skypunk)
 end
 
 
-print("Initializing!", 10, 10, 15)
 init()
 
-
 function TIC()
-
 	-- clear the screen
 	cls(10)
 
 	for i, e in ipairs(ents) do
+		ents[i]:think()
 		ents[i]:animate()
+		ents[i]:act()
+		ents[i]:phys()
 		ents[i]:draw()
 	end
 end
